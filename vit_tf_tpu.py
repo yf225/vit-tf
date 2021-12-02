0b3581d57cadb13a33642f26010c9a966157a4f3
Will Feng (DPER) <willfeng@fb.com>
2021-12-01 17:44:41 -0800

    update

--- vit_tf_tpu.py
+++ vit_tf_tpu.py
@@ -10,7 +10,7 @@ cd ./vit-tf
 
 # Copy this file content to vit.py on TPU VM.
 # Run the file:
-python3 vit_tf_gpu.py --bits=16 --micro_batch_size=4
+python3 vit_tf_tpu.py --bits=16 --micro_batch_size=4
 """
 
 # !pip install -U tensorflow-addons
