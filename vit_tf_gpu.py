37a0f56d1198a4b4dfc0ce6d2142964b1627334a
Will Feng (DPER) <willfeng@fb.com>
2021-12-02 23:11:59 -0800

    update

--- vit_tf_gpu.py
+++ vit_tf_gpu.py
@@ -23,11 +23,11 @@ git clone https://github.com/yf225/vit-tf.git
 
 cd ./vit-tf
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 TF_XLA_FLAGS=--tf_xla_auto_jit=2 python3 vit_tf_gpu.py --bits=16 --micro_batch_size=4
+CUDA_VISIBLE_DEVICES=0,1,2,3 TF_XLA_FLAGS="--tf_xla_enable_xla_devices --tf_xla_auto_jit=2" python3 vit_tf_gpu.py --bits=16 --micro_batch_size=4
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 TF_XLA_FLAGS=--tf_xla_auto_jit=-1 python3 vit_tf_gpu.py --bits=16 --micro_batch_size=4
+CUDA_VISIBLE_DEVICES=0,1,2,3 TF_XLA_FLAGS="--tf_xla_enable_xla_devices --tf_xla_auto_jit=-1" python3 vit_tf_gpu.py --bits=16 --micro_batch_size=4
 
-CUDA_VISIBLE_DEVICES=0 TF_XLA_FLAGS=--tf_xla_auto_jit=-1 python3 vit_tf_gpu.py --bits=16 --micro_batch_size=4
+CUDA_VISIBLE_DEVICES=0 TF_XLA_FLAGS="--tf_xla_enable_xla_devices --tf_xla_auto_jit=-1" python3 vit_tf_gpu.py --bits=16 --micro_batch_size=4
 """
 
 # -*- coding: utf-8 -*-
